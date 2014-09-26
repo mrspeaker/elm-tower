e6333ac7b6128e61eaa5c969532dc88fa51c17a2
Mr Speaker <mrspeaker@gmail.com>
2014-09-26 19:08:28 -0400

    more debug

--- elmTower/ElmTower.elm
+++ elmTower/ElmTower.elm
@@ -17,7 +17,7 @@ data State = Waiting | Playing | Dead
 -- initial state
 defaultGame = {
   tick = 0,
-  player = { x=0, y=0, w=35, h=35, vx=0, vy=0, dir="right", colour = (rgb 0 0 0), isFalling = False},
+  player = { x=0, y=0, w=16, h=28, vx=0, vy=0, dir="right", colour = (rgb 0 0 0), isFalling = False},
   platforms = createPlatforms,
   state = Playing }
 
@@ -26,16 +26,9 @@ createPlatforms = map (\n -> {
   x = 30 * n + 10, 
   y = toFloat ((50 * n + 10) % 280),
   w = 60,
-  h = 10}) tenToOne
---createPlatforms = [
---    {x = 10, y = 0, w = 60, h = 10},
---    {x = 30, y = 50, w = 60, h = 10},
---    {x = 50, y = 100, w = 60, h = 10},
---    {x = 70, y = 150, w = 60, h = 10},
---    {x = 90, y = 200, w = 60, h = 10}]
+  h = 10}) tenToOne ++ [{x = 0, y = -38, w = 10000, h = 50}]
 
--- update player --
-jump {y} m = if y > 0 && (not m.isFalling || m.y == 0) then { m | vy <- 6} else m -- , isFalling <- True 
+jump {y} m = if y > 0 && (not m.isFalling || m.y == 0) then { m | vy <- 6} else m
 gravity t m = if m.y > 0 && m.isFalling then { m | vy <- m.vy - t/4 } else m
 physics t m = { m | x <- m.x + t*m.vx , y <- max 0 (m.y + t*m.vy) }
 walk {x} m = { m | vx <- toFloat x * 2
@@ -43,69 +36,93 @@ walk {x} m = { m | vx <- toFloat x * 2
                              | x > 0     -> "right"
                              | otherwise -> m.dir }
 
-step: [Platform] -> (Float, {x: Int, y: Int}) -> Player -> Player
-step platforms (dt, keys) =
-  let current = jump keys >> gravity dt >> walk keys >> physics dt
-  in (\player -> 
-    let
-      currentPlayer = current(player)
-      a = {currentPlayer |x <- currentPlayer.x - currentPlayer.w / 2
-                         ,y <- currentPlayer.y - currentPlayer.h / 2}
-      collidedPlatforms = filter (\plat -> 
-        let b = {plat |x <- plat.x - plat.w / 2
-                      ,y <- plat.y - plat.h / 2}
-        in a.x + a.w >= b.x && a.x <= b.x + b.w && a.y + a.h >= b.y && a.y <= b.y + b.h ) platforms
-    in if (isEmpty collidedPlatforms) 
-       then {currentPlayer | colour <- (rgb 0 0 0), isFalling <- True} 
-       else {currentPlayer | y <- currentPlayer.y
-                           , colour <- (rgb 255 255 0)
-                           , vy <- 0
-                           , isFalling <- False})
-       --a)
-  
-stepPlatforms platforms tick = map (\p -> {p | x <- p.x + sin (toFloat tick / 10) * 1 }) platforms
-
-stepGame : (Float, {x: Int, y: Int}) -> Game -> Game
-stepGame input game = 
-  { game | tick <- 1 + game.tick
-         , player <- step game.platforms input game.player
-         , platforms <- stepPlatforms game.platforms game.tick
-         , state <- game.state}
-
 renderPlatform : Platform -> Form
 renderPlatform platform = rect platform.w platform.h |> filled (rgb 124 200 100) 
                                                      |> move (platform.x, platform.y)
-
---platform : Float -> Float -> Float -> Int -> Form
---platform w h n tick = rect 50 10 |> filled (rgb 124 200 100) 
---                             |> move(-w / 2 + (20 * n) + 200 + sin((toFloat tick * n) / 300) * 65, -h / 2 + (60 * n))
                              
 tenToOne = let (_, l) = foldl (\j (i, acc) -> (i+1, (i + 1) :: acc)) (0, []) (repeat 10 1) in l
 
 renderPlatforms platforms = map (\plat -> renderPlatform plat) platforms
 
 
-gameState = foldp stepGame defaultGame input 
+-- Mario SIgnal
+marioStateChange: (Float, {x: Int, y: Int}) -> Player -> Player
+marioStateChange (dt, keys) = jump keys >> gravity dt >> walk keys >> physics dt
+
+stepMario : (Float, {x: Int, y: Int}) -> [Player] -> [Player]
+stepMario input marioStates =
+  let lastestMarioState = head marioStates
+      (_ , keys) = input
+  in (if keys.y >= 0 
+      then [marioStateChange input lastestMarioState] ++ marioStates
+      else tail (tail marioStates))
+
+marioSignal = foldp stepMario [defaultGame.player] input
+--
+
+-- PlatformSignal
+stepPlatform: (Float, {x: Int, y: Int}) -> [Platform] -> [Platform]
+stepPlatform input platforms = platforms
+
+platformSignal = foldp stepPlatform createPlatforms input
+--
+
+--
+isCollided currentPlayer plat =
+  let 
+      a = {currentPlayer |x <- currentPlayer.x - currentPlayer.w / 2
+                         ,y <- currentPlayer.y - currentPlayer.h / 2}
+      b = {plat |x <- plat.x - plat.w / 2
+               ,y <- plat.y - plat.h / 2}
+      isLeftCollision = a.x + a.w >= b.x
+      isRightCollision = a.x <= b.x + b.w
+      isBottomCollision = a.y + a.h >= b.y
+      isTopCollision = a.y <= b.y + b.h
+  in isLeftCollision && isRightCollision && isBottomCollision && isTopCollision
+
+combineToCollidedPlayer : [Player] -> [Platform] -> [Player]
+combineToCollidedPlayer (mario :: restMarios) platforms = 
+  let collidedPlatforms = filter (isCollided mario) platforms
+      newPlayer = if (isEmpty collidedPlatforms) 
+                  then {mario | colour <- (rgb 0 0 0), isFalling <- True} 
+                  else {mario | colour <- (rgb 255 255 0)
+                              , vy <- 0
+                              , isFalling <- False}
+  in newPlayer :: restMarios
+
+collidedPlayerSignal = lift2 combineToCollidedPlayer marioSignal platformSignal
+
+-- Collision detenction including mario signal 
+
+stepGameState: [Player] -> [Game] -> [Game]
+stepGameState (mario :: restMarios) (gameState :: restGameSates) = 
+  {gameState | tick <- gameState.tick + 1
+             , player <- mario
+             , platforms <- gameState.platforms
+             , state <- gameState.state} :: restGameSates
+
+gameStateSignal = foldp stepGameState [defaultGame] collidedPlayerSignal
+
+-- 
 
 
 -- DISPLAY
 
-render (w',h') gameState =
+render (w',h') (gameState :: _) =
   let (w,h) = (toFloat w', toFloat h')
-      verb = if | gameState.player.y  >  0 -> "jump"
+      verb = if | gameState.player.isFalling -> "jump"
                 | gameState.player.vx /= 0 -> "walk"
                 | otherwise     -> "stand"
       src = "/imgs/mario/" ++ verb ++ "/" ++ gameState.player.dir ++ ".gif"
       player = gameState.player
+      col = if gameState.player.vy == 0 then rgb 255 0 0 else rgb 0 0 255
   in collage w' h'
       ([ rect w h  |> filled (rgb 174 238 238),
-        rect w 50 |> filled (rgb 74 63 41)
-                  |> move (0, -42),
-        rect player.w player.h |> filled player.colour |> move (player.x, player.y),
+        rect player.w player.h |> filled col |> move (player.x, player.y),
         toForm (image 35 35 src) |> move (player.x, player.y)
-      ] ++ (renderPlatforms gameState.platforms))
+      ] ++ (renderPlatforms gameState.platforms) ++ [rect w 50 |> filled (rgb 74 63 41) |> move (0, -38)])
 
 input = let delta = lift (\t -> t/20) (fps 60)
         in  sampleOn delta (lift2 (,) delta Keyboard.arrows)
 
-main = lift2 render Window.dimensions gameState
+main = lift2 render Window.dimensions gameStateSignal
