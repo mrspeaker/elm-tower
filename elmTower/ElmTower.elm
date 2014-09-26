59150c2acf1a168071fa9fa7ff8a313bd08d128b
Mr Speaker <mrspeaker@gmail.com>
2014-09-26 13:56:57 -0400

    multiple platforms

--- elmTower/ElmTower.elm
+++ elmTower/ElmTower.elm
@@ -42,6 +42,14 @@ stepGame input game =
          , platforms <- game.platforms
          , state <- game.state}
 
+platform : Float -> Float -> Float -> Form
+platform w h n = rect 100 10 |> filled (rgb 24 0 0) 
+                             |> move(-w / 2 + 100, -h / 2 + (100 * n))
+                             
+platformblah = let (_, l) = foldl (\j (i, acc) -> (i+1, (i + 1) :: acc)) (0, []) (repeat 10 1) in l
+
+platforms w h = map (\n -> platform w h n) platformblah
+
 
 gameState = foldp stepGame defaultGame input 
 
@@ -56,15 +64,13 @@ render (w',h') gameState =
       src = "/imgs/mario/" ++ verb ++ "/" ++ gameState.player.dir ++ ".gif"
       mario = gameState.player
   in collage w' h'
-      [ rect w h  |> filled (rgb 174 238 238),
+      ([ rect w h  |> filled (rgb 174 238 238),
         rect w 50 |> filled (rgb 74 63 141)
                   |> move (0, 24 - h/2),
-        rect 100 10 |> filled (rgb 24 0 0) 
-                    |> move(-w / 2 + 100, -h / 2 + 160),
         ngon ((round mario.x) % 10) 25.0  |> filled (rgb 24 0 0) 
                                           |> move(-w / 2 + 300, -h / 2 + 160),
         toForm (image 35 35 src) |> move (mario.x, mario.y + 62 - h/2)
-      ]
+      ] ++ (platforms w h))
 
 -- MARIO
 input = let delta = lift (\t -> t/20) (fps 60)
