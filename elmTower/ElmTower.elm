bfdf36b2f5013d2e7a0c3f03c9d04e5f3b99aac1
Mr Speaker <mrspeaker@gmail.com>
2014-09-27 18:18:39 -0400

    not workin note

--- elmTower/ElmTower.elm
+++ elmTower/ElmTower.elm
@@ -1,7 +1,32 @@
 import Keyboard
 import Window
 import Color
-import Debug
+
+{--
+
+Note on why it's the multi-signal version is not working!
+
+The updated player from collidedPlayerSignal never gets passed back to playerSignal...
+
+We have have four streams in total:
+
+playerSignal = foldp stepPlayer [defaultGame.player] input
+platformSignal = foldp stepPlatform defaultGame.platforms input
+collidedPlayerSignal = lift2 checkCollisions platformSignal playerSignal
+gameStateSignal = foldp stepGameState [defaultGame] collidedPlayerSignal
+
+The playerSignal does the gravity/physics, then is sent with platfromSignal to be checked for collisions. 
+The collidedPlayerSignal gets the player object from the playerSignal. If there is a collision, 
+it sets the isFalling to true and replaces it in the signal.
+
+This signal is then sent to the gameState signal for rendering: and so it renders 
+correctly (isFalling is correct).
+
+Buuut! The next frame, it starts again - and the playerSignal takes the player - but it's not 
+the player from the collidedPlayerSignal, it's the player from the playerSignal: so the 
+isFalling (and color) changing that is done in collidedPlayerSignal is never passed back.
+
+--}
 
 -- MODELs
 type Platform = { x: Float, y: Float, w: Float, h: Float }
@@ -66,10 +91,7 @@ isCollided entityA entityB =
   in isLeftGreater && isRightLess && isBottomGreater && isTopLess
 
 jump {y} m = if y > 0 && (not m.isFalling || m.y == 0) then { m | vy <- 6} else m
-gravity t m = 
-  let falling = Debug.watch("falling") m.isFalling
-      vx = Debug.watch ("vy") m.y
-  in if m.y > 0 && m.isFalling then { m | vy <- m.vy - t/4 } else m
+gravity t m = if m.y > 0 && m.isFalling then { m | vy <- m.vy - t/4 } else m
 physics t m = { m | x <- m.x + t*m.vx , y <- max 0 (m.y + t*m.vy) }
 walk {x} m = { m | vx <- toFloat x * 2
                  , dir <- if | x < 0     -> "left"
@@ -81,13 +103,13 @@ playerStateChange: Input -> Player -> Player
 playerStateChange (dt, keys) = jump keys >> gravity dt >> walk keys >> physics dt
 
 stepPlayer : Input -> [Player] -> [Player]
-stepPlayer input players =
-  let latestPlayerState = head players
+stepPlayer input playerStates =
+  let latestPlayerState = head playerStates
       (_ , keys) = input
   -- If playing, save state else if holding "down" key, then "rewind"
   in (if keys.y >= 0 
-      then (playerStateChange input latestPlayerState) :: players
-      else tail (tail players))
+      then [playerStateChange input latestPlayerState] ++ playerStates
+      else tail (tail playerStates))
 
 playerSignal = foldp stepPlayer [defaultGame.player] input
 --
@@ -100,28 +122,24 @@ platformSignal = foldp stepPlatform defaultGame.platforms input
 --
 
 --
-checkCollisions :  [Platform] -> [Player] -> [Player]
-checkCollisions platforms players = 
-  let player = head players
-      restPlayers = tail players
-      collidedPlatforms = filter (isCollided player) platforms
-      fall2 = Debug.watch("Fall12") (length players)
+checkCollisions : [Player] -> [Platform] -> [Player]
+checkCollisions (player :: restPlayers) platforms = 
+  let collidedPlatforms = filter (isCollided player) platforms
       newPlayer = if (isEmpty collidedPlatforms) 
                   then { player | colour <- (rgb 0 0 0)
                                 , isFalling <- True } 
                   else { player | colour <- (rgb 255 255 0)
                                 , vy <- 0
                                 , isFalling <- False}
-  in (newPlayer :: restPlayers)
+  in newPlayer :: restPlayers
 
-collidedPlayerSignal = lift2 checkCollisions platformSignal playerSignal
+collidedPlayerSignal = checkCollisions <~ playerSignal ~ platformSignal
 
 -- Collision detenction including player signal 
 
 stepGameState: [Player] -> [Game] -> [Game]
 stepGameState (player :: _) (gameState :: restGameSates) = 
-  let playerer = Debug.watch("ph") player.isFalling
-  in {gameState | tick <- gameState.tick + 1
+  {gameState | tick <- gameState.tick + 1
              , player <- player
              , platforms <- gameState.platforms
              , state <- gameState.state} :: restGameSates
@@ -150,7 +168,7 @@ render (w',h') (gameState :: _) =
   in collage w' h'
       ([ rect w h  |> filled (rgb 174 238 238),
         rect player.w player.h |> filled col |> move (player.x, player.y),
-        toForm (image 35 35 src) |> Debug.trace("playerpath") |> move (player.x, player.y)
+        toForm (image 35 35 src) |> move (player.x, player.y)
       ] ++ (renderPlatforms gameState.platforms) ++ (renderGround w h))
 
 input = let delta = lift (\t -> t/20) (fps 60)
